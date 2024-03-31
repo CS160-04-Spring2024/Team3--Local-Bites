1ee26d14f756075159d019e97744ecb753974bb8
Shivansh <shan18u@users.noreply.github.com>
2024-03-31 15:54:38 -0700

    routing

--- src/app/basic/services/auth/auth.service.ts
+++ src/app/basic/services/auth/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const BASIC_URL = 'http://localhost:8080/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+
+  registerClient(SignupRequestDTO:any): Observable<any>{
+    return this.http.post(BASIC_URL + "client/sign-up", SignupRequestDTO);
+  }
+
+  registerCompany(SignupRequestDTO:any): Observable<any>{
+    return this.http.post(BASIC_URL + "company/sign-up", SignupRequestDTO);
+  }
+
 }
